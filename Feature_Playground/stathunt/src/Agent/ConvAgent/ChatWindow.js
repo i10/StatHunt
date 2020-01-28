27f7a8bf45374cb83bc31d303f18235cf2b24550
Malte Adrian Xiang Rui Meng <malte.meng@rwth-aachen.de>
2020-01-28 13:57:54 +0100

    chat interaction from script

--- Feature_Playground/stathunt/src/Agent/ConvAgent/ChatWindow.js
+++ Feature_Playground/stathunt/src/Agent/ConvAgent/ChatWindow.js
@@ -6,29 +6,24 @@ import ChatBot from 'react-simple-chatbot';
 export default class ChatWindow extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            steps: [
-                {
-                    id: '0',
-                    message: "Welcome to StatHunt!",
-                    trigger: '1'
-                },
-                {
-                    id: '1',
-                    message: "I'm Hunter, your guide to finding a statistical analysis procedure",
-                    trigger: '2'
-                },
-                {  
-                    id: '2',
-                    message: "Let's get started, what's the name of your experiment?",
-                    trigger: '3'
-                },
-                {
-                    id: '3',
-                    user: true,
-                    trigger: '3'
+        var input = "Hello, welcome to StatHunt! I’m here to help you find a correct statistical procedure for your experiment./ If you’re unsure how to respond, just send me a ‘not sure’ or ‘help’./What’s the name of the experiment you need help with?&What are you trying to achieve with the experiment?&What is the research hypothesis?/ A hypothesis is a sentence that states a relationship between two or more variables. E.g., “Users who use system A performed the task faster than users who use system B.”&OK, let’s get to the experimental design. Will you be using a between-subjects or within-subjects design?&A between-subjects study design would mean that each user group uses only one condition of the independent variable, whereas a within-subjects study design would mean that every user group uses all conditions.&"
+        var l = 0;
+        var steps = [];
+        var step = 0;
+        for (var r = 0; r < input.length; r++){
+            if(input[r] == '&' || input[r] =='/'){
+                steps[step] = {id: String(step), message: input.slice(l, r), trigger: String(step+1)};
+                l = r+1;
+                step++;
+                if(input[r] == '&'){
+                    steps[step] = {id: String(step), user: true, trigger: (r +1 == input.length) ? String(step) : String(step+1)};
+                    step++;
                 }
-            ]
+            }
+        }
+
+        this.state = {
+            steps: steps
         }
     }
 
