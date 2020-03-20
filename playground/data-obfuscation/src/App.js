d3cd9f9e7e6bf984aa0ce650df1f50d0da6d5a37
Malte Adrian Xiang Rui Meng <malte.meng@rwth-aachen.de>
2020-03-20 13:46:24 +0100

    laplace noise

--- playground/data-obfuscation/src/App.js
+++ playground/data-obfuscation/src/App.js
@@ -1,248 +1,45 @@
 import React, { Component } from 'react';
-import { ResponsiveBar } from '@nivo/bar';
-import MaterialTable from 'material-table';
-import Grid from '@material-ui/core/Grid';
-import { Container, Stepper, Step, StepLabel, Typography } from '@material-ui/core';
-import HorizontalNonLinearStepper from './NavigationStepper';
-import ChatWindow from './ChatWindow';
-import Graph from './Graph';
+import { randomNormal } from 'd3-random';
+import { Grid, Slider } from '@material-ui/core/';
 
 export default class App extends Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state = {
+      epsilon: 0
+    }
+  }
+  getDistr() {
+    var normal = Array.from(Array(1000).keys(), x => ([x, randomNormal(100, 10)()]));
+    console.log(normal);
+    return normal;
+  }
 
-    var data = [
-      {
-        "country": "AD",
-        "hot dog": 63,
-        "hot dogColor": "hsl(188, 70%, 50%)",
-        "burger": 21,
-        "burgerColor": "hsl(339, 70%, 50%)",
-        "sandwich": 15,
-        "sandwichColor": "hsl(32, 70%, 50%)",
-        "kebab": 198,
-        "kebabColor": "hsl(141, 70%, 50%)",
-        "fries": 112,
-        "friesColor": "hsl(0, 70%, 50%)",
-        "donut": 28,
-        "donutColor": "hsl(248, 70%, 50%)"
-      },
-      {
-        "country": "AE",
-        "hot dog": 134,
-        "hot dogColor": "hsl(226, 70%, 50%)",
-        "burger": 105,
-        "burgerColor": "hsl(204, 70%, 50%)",
-        "sandwich": 168,
-        "sandwichColor": "hsl(153, 70%, 50%)",
-        "kebab": 78,
-        "kebabColor": "hsl(31, 70%, 50%)",
-        "fries": 134,
-        "friesColor": "hsl(46, 70%, 50%)",
-        "donut": 198,
-        "donutColor": "hsl(75, 70%, 50%)"
-      },
-      {
-        "country": "AF",
-        "hot dog": 98,
-        "hot dogColor": "hsl(101, 70%, 50%)",
-        "burger": 7,
-        "burgerColor": "hsl(267, 70%, 50%)",
-        "sandwich": 185,
-        "sandwichColor": "hsl(87, 70%, 50%)",
-        "kebab": 22,
-        "kebabColor": "hsl(135, 70%, 50%)",
-        "fries": 170,
-        "friesColor": "hsl(247, 70%, 50%)",
-        "donut": 45,
-        "donutColor": "hsl(273, 70%, 50%)"
-      },
-      {
-        "country": "AG",
-        "hot dog": 149,
-        "hot dogColor": "hsl(109, 70%, 50%)",
-        "burger": 194,
-        "burgerColor": "hsl(221, 70%, 50%)",
-        "sandwich": 133,
-        "sandwichColor": "hsl(293, 70%, 50%)",
-        "kebab": 39,
-        "kebabColor": "hsl(351, 70%, 50%)",
-        "fries": 27,
-        "friesColor": "hsl(51, 70%, 50%)",
-        "donut": 65,
-        "donutColor": "hsl(129, 70%, 50%)"
-      },
-      {
-        "country": "AI",
-        "hot dog": 80,
-        "hot dogColor": "hsl(137, 70%, 50%)",
-        "burger": 13,
-        "burgerColor": "hsl(286, 70%, 50%)",
-        "sandwich": 42,
-        "sandwichColor": "hsl(57, 70%, 50%)",
-        "kebab": 169,
-        "kebabColor": "hsl(201, 70%, 50%)",
-        "fries": 94,
-        "friesColor": "hsl(276, 70%, 50%)",
-        "donut": 46,
-        "donutColor": "hsl(358, 70%, 50%)"
-      },
-      {
-        "country": "AL",
-        "hot dog": 47,
-        "hot dogColor": "hsl(65, 70%, 50%)",
-        "burger": 54,
-        "burgerColor": "hsl(71, 70%, 50%)",
-        "sandwich": 98,
-        "sandwichColor": "hsl(139, 70%, 50%)",
-        "kebab": 168,
-        "kebabColor": "hsl(321, 70%, 50%)",
-        "fries": 98,
-        "friesColor": "hsl(229, 70%, 50%)",
-        "donut": 162,
-        "donutColor": "hsl(292, 70%, 50%)"
-      },
-      {
-        "country": "AM",
-        "hot dog": 25,
-        "hot dogColor": "hsl(285, 70%, 50%)",
-        "burger": 7,
-        "burgerColor": "hsl(156, 70%, 50%)",
-        "sandwich": 117,
-        "sandwichColor": "hsl(51, 70%, 50%)",
-        "kebab": 27,
-        "kebabColor": "hsl(337, 70%, 50%)",
-        "fries": 103,
-        "friesColor": "hsl(271, 70%, 50%)",
-        "donut": 181,
-        "donutColor": "hsl(325, 70%, 50%)"
-      }
-    ];
-    const graph = <ResponsiveBar
-      data={data}
-      keys={['hot dog', 'burger', 'sandwich', 'kebab', 'fries', 'donut']}
-      indexBy="country"
-      margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-      padding={0.3}
-      colors={{ scheme: 'nivo' }}
-      defs={[
-        {
-          id: 'dots',
-          type: 'patternDots',
-          background: 'inherit',
-          color: '#38bcb2',
-          size: 4,
-          padding: 1,
-          stagger: true
-        },
-        {
-          id: 'lines',
-          type: 'patternLines',
-          background: 'inherit',
-          color: '#eed312',
-          rotation: -45,
-          lineWidth: 6,
-          spacing: 10
-        }
-      ]}
-      fill={[
-        {
-          match: {
-            id: 'fries'
-          },
-          id: 'dots'
-        },
-        {
-          match: {
-            id: 'sandwich'
-          },
-          id: 'lines'
-        }
-      ]}
-      borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: 'country',
-        legendPosition: 'middle',
-        legendOffset: 32
-      }}
-      axisLeft={{
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: 'food',
-        legendPosition: 'middle',
-        legendOffset: -40
-      }}
-      labelSkipWidth={12}
-      labelSkipHeight={12}
-      labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
-      legends={[
-        {
-          dataFrom: 'keys',
-          anchor: 'bottom-right',
-          direction: 'column',
-          justify: false,
-          translateX: 120,
-          translateY: 0,
-          itemsSpacing: 2,
-          itemWidth: 100,
-          itemHeight: 20,
-          itemDirection: 'left-to-right',
-          itemOpacity: 0.85,
-          symbolSize: 20,
-          effects: [
-            {
-              on: 'hover',
-              style: {
-                itemOpacity: 1
-              }
-            }
-          ]
-        }
-      ]}
-      animate={true}
-      motionStiffness={90}
-      motionDamping={15}
-    />;
+  obfuscate(val, eps) {
+    const mu = -.5;
+    const b = eps;
+    var u = Math.random() - 0.5;
+    return val + (mu - b * (u < 0 ? -1 : 1) * Math.log(1 - 2 * Math.abs(u)));
+  }
+  render() {
     return <React.Fragment>
       <Grid container>
-        <Grid xs={2} item>
-          <Container>
-            <Typography variant='h4' align='center' display='block' style={{padding:'10px'}}>
-              Stathunt
-          </Typography>
-          </Container>
-
+        <Grid item xs={2}>
+          <Slider value={this.state.epsilon} onChange={(e, newVal) => this.setState({epsilon: newVal})}/>
         </Grid>
-        <Grid xs={10} item>
-          <HorizontalNonLinearStepper />
+        <Grid item xs={8}>
+          <table>
+            <tr><td>Part ID</td><td>Values</td><td>Obfuscated Values</td></tr>
+            {this.getDistr().map((el) =>
+              <tr><td>{el[0]}</td>
+                <td>{el[1]}</td>
+                <td>{this.obfuscate(el[1], this.state.epsilon)}</td></tr>)}
+          </table>
         </Grid>
-      </Grid>
-
-      <Grid container>
-        <Grid container xs={8}>
-          <Grid item xs={12} style={{ height: '40vh' }}>
-            <Graph />
-          </Grid>
-          <Grid item xs={12} style={{ height: '50vh' }}>
-            <MaterialTable columns={[
-              { title: 'Country', field: 'country' },
-              { title: 'Hot Dog', field: 'hot dog' },
-              { title: 'Burger', field: 'burger' }
-            ]}
-              data={data} />
-          </Grid>
-        </Grid>
-        <Grid item xs={4}>
-          <ChatWindow />
+        <Grid item xs={2}>
         </Grid>
       </Grid>
+
     </React.Fragment>
-
-
   }
 }
